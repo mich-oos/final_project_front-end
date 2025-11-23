ab85a223f931ab3dd88a8e22635fe3e6cde5a894
Astrid Mongi <12410384@student.unklab.ac.id>
2025-11-23 17:33:28 +0800

    membuat footer

---
+++ src/ProjectFinal/home/footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+return (
+<footer className="bg-[#F6F6F6] p-4 text-center mt-10 shadow-inner">
+<p className="font-semibold text-lg">Vastly Fashion</p>
+<p className="text-sm text-gray-600">© 2024 All Rights Reserved</p>
+<div className="flex justify-center gap-6 mt-2 text-sm text-gray-700">
+<p>Email: support@vastlyfashion.com</p>
+<p>Instagram: @vastlyfashion</p>
+</div>
+</footer>
+);
+};
+
+
+export default Footer; 
+
